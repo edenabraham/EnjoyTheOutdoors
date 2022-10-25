b126f34ac95ea4d65158f9d31400f6a6373b0641
Eden Abraham <eabraham@atl.yearup.org>
2022-10-25 19:24:33 -0400

    set up nav

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+const navURLs= [
+    {label: "Home", url: "index.html"},
+    {label: "National Parks", url: "parks.html"},
+    {label: "Mountains", url: "mountains.html"},
+];
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const nav= document.getElementById("nav");
+    navURLs.forEach(item => nav.innerHTML += `<a href="${item.url}">${item.label}</a>`);
+    
+});
