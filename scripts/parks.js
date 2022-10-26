a4aa844aa451d8e31d0122b5b6f37b42cd62870a
Eden Abraham <eabraham@atl.yearup.org>
2022-10-26 00:02:36 -0400

    national park filter

--- scripts/parks.js
+++ scripts/parks.js
@@ -5,21 +5,28 @@ function locationOption(item){
     option.innerHTML= item;
     return option;
 }
-
+function parkCard(item){
+    const card= document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML= item.LocationName;
+    return card
+}
+function showCards(list, target) {
+    target.innerHTML= "";
+    list.forEach(item => results.appendChild(parkCard(item)));
+}
 
 document.addEventListener("DOMContentLoaded", () => {
+    const results= document.getElementById("results");
     const select= document.getElementById("location");
     locationsArray.forEach(item => select.appendChild(locationOption(item)));
+    
     select.addEventListener("change", (e) =>{
-        alert(select.selectedOptions[0].value)
+        
+        const v= select.selectedOptions[0].value;
+        const matches= nationalParksArray.filter(item => item.State == v);
+        showCards(matches,results);
     })
-
-    function parkCard(item){
-        const card= document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML= item.LocationName;
-        return card
-    }
-    const results= document.getElementById("results");
-    nationalParksArray.forEach(item => results.appendChild(parkCard(item)))
+    // shows all cards
+    //showCards(nationalParksArray,results);
 });
