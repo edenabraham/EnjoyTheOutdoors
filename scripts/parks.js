fa283a8e0541216d0f4b30f0969edcc710df2ce4
Eden Abraham <eabraham@atl.yearup.org>
2022-10-25 20:29:29 -0400

    added national park data

--- scripts/parks.js
+++ scripts/parks.js
@@ -13,4 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
     select.addEventListener("change", (e) =>{
         alert(select.selectedOptions[0].value)
     })
+
+    function parkCard(item){
+        const card= document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML= item.LocationName;
+        return card
+    }
+    const results= document.getElementById("results");
+    nationalParksArray.forEach(item => results.appendChild(parkCard(item)))
 });
